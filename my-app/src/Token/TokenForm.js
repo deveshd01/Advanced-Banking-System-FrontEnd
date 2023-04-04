df9e04d73c5ff31d4283aa820ce905c3c834fb1d
100rabh patil <99138605+100rab100@users.noreply.github.com>
2023-04-04 11:28:33 +0530

    Update TokenForm.js

--- my-app/src/Token/TokenForm.js
+++ my-app/src/Token/TokenForm.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import './Token.css'; // Import the CSS file
 
+const serviceOptions = [
+  "Deposit",
+  "Withdrawal",
+  "Balance Inquiry",
+  "Transfer",
+  "Create Account",
+  "Delete Account",
+  "Catch All Counter",
+  "Passbook Print",
+  "Statement Print",
+];
+
 const TokenForm = () => {
   const [selectedServices, setSelectedServices] = useState([]); // Initialize the state variable for the selected services
   const [message, setMessage] = useState(""); // Initialize the state variable for the message to be displayed
@@ -32,42 +44,31 @@ const TokenForm = () => {
           &nbsp;&nbsp;
         </div>
         <div className="services-row">
-          <Button className={`service-button ${selectedServices.includes("deposit") ? "selected" : ""}`} onClick={() => handleServiceClick("deposit")}>Deposit</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("withdrawal") ? "selected" : ""}`} onClick={() => handleServiceClick("withdrawal")}>Withdrawal</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("balance inquiry") ? "selected" : ""}`} onClick={() => handleServiceClick("balance inquiry")}>Balance Inquiry</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("transfer") ? "selected" : ""}`} onClick={() => handleServiceClick("transfer")}>Transfer</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("Create Account") ? "selected" : ""}`} onClick={() => handleServiceClick("Create Account")}>Create Account</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("Delete Account") ? "selected" : ""}`} onClick={() => handleServiceClick("Delete Account")}>Delete Account</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("catch all counter") ? "selected" : ""}`} onClick={() => handleServiceClick("catch all counter")}>Catch All Counter</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("Passbook Print") ? "selected" : ""}`} onClick={() => handleServiceClick("Passbook Print")}>Passbook Print</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <Button className={`service-button ${selectedServices.includes("Statement print") ? "selected" : ""}`} onClick={() => handleServiceClick("Statement print")}>Statement print</Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-
-  
-</div>
+          {serviceOptions.map((service) => (
+            <Button
+              key={service}
+              className={`service-button ${selectedServices.includes(service) ? "selected" : ""}`}
+              onClick={() => handleServiceClick(service)}
+            >
+              {service}
+            </Button>
+          ))}
+        </div>
 
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <form onSubmit={handleFormSubmit}>
         <div className="submit">
-  <Button
-    name="submit"
-    type="submit"
-    id="submit"
-    style={{ width: '100px' }}
-  >
-    Generate Token
-  </Button>
-</div>
-
+          <Button
+            name="submit"
+            type="submit"
+            id="submit"
+            style={{ width: '100px' }}
+          >
+            Generate Token
+          </Button>
+        </div>
         </form>
+
         {message && <p className="message">{message}</p>} {/* Display the message if there is one */}
       </div>
     </div>
